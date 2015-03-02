ee1f2b32e55632c9b540bc76f04e94d59dfc9d09
CalvineHicks <calvinehicks@gmail.com>
2015-03-01 20:34:56 -0700

    updated .gitignore

---
+++ FlorasphereNEW/app/src/main/java/com/example/florasphere/FlorasphereListActivity.java
@@ -0,0 +1,17 @@
+package com.example.florasphere;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+/**
+ * Created by calvineh on 3/1/15.
+ */
+public class FlorasphereListActivity extends ListActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ FlorasphereNEW/app/src/main/java/com/example/florasphere/FlorasphereListArrayAdaptor.java
@@ -0,0 +1,23 @@
+package com.example.florasphere;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by calvineh on 3/1/15.
+ */
+//public class FlorasphereListArrayAdaptor  extends ArrayAdapter<String>{
+
+//    public FlorasphereListArrayAdapter(Context context,String[] values	)
+//    {
+//        super(context, R.layout.list_suggestedsnacks, values);
+//        this.context = context;
+//        this.values = values;
+//        dh = new DatabaseHelper(context);
+//    }
+//    public View getView(int position, View convertView, ViewGroup parent)
+//    {
+//        return View;
+//    }
+//}
