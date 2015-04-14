ed1e1dff00aa1a6295b57671a42e29c74483c43d
Madison Rockwell <madisonrockwell@engr2gw-4-229-dhcp.int.colorado.edu>
2015-04-14 11:52:24 -0600

    added Contact Us button inside Settings page

--- FlorasphereNEW/app/src/main/java/com/example/florasphere/SettingsActivity.java
+++ FlorasphereNEW/app/src/main/java/com/example/florasphere/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.example.florasphere;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.app.*;
 import android.util.Log;
@@ -7,12 +8,23 @@ import android.view.*;
 import android.widget.*;
 import android.content.Intent;
 
+
 /**
  * Created by madisonrockwell on 3/15/15.
  */
 public class SettingsActivity extends Activity{
+
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+
+        Button contactUs = (Button) findViewById(R.id.activity_contact_us);
+        contactUs.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent k = new Intent(SettingsActivity.this, ContactUs.class);
+                startActivity(k);
+                Log.i("tag", "Contact Us button pressed, ContactUs started");
+            }
+        });
     }
 }
