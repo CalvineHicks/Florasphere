dd41fd94f36fd03bb5842c773547e85aa1517c65
CalvineHicks <calvinehicks@gmail.com>
2015-04-18 20:01:56 -0600

    adding jess' changes

--- FlorasphereNEW/app/src/main/java/com/example/florasphere/PlantDatabase.java
+++ FlorasphereNEW/app/src/main/java/com/example/florasphere/PlantDatabase.java
@@ -1,4 +1,3 @@
-
 package com.example.florasphere;
 
 import android.annotation.TargetApi;
@@ -6,11 +5,10 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Build;
-
-import java.net.MalformedURLException;
-import java.net.URL;
+import android.util.Log;
 
 /**
  * Creates a single local database that is located on user's phone.
@@ -19,16 +17,16 @@ public class PlantDatabase extends SQLiteOpenHelper
 {
     private static PlantDatabase instance = null;
     private static final int DATABASE_VERSION = 1;
-    private static final String TABLE_NAME = "Plant Database";
+    private static final String TABLE_NAME = "PlantDatabase";
     private static SQLiteDatabase.CursorFactory factory = null;
     private static final String TABLE_CREATE =
-            "CREATE TABLE " + TABLE_NAME + " ("   +
-                    "PLANT_NAME" + " TEXT, "      +
-                    "PLANT_PIC"  + " HYPERLINK, " +
-                    "WATER_FREQ" + " INTEGER, "   +
-                    "WATER_AMT"  + " TEXT, "      +
-                    "LIGHT_AMT"  + " TEXT, "      +
-                    "GEN_INFO"   + " MEMO "       + ");";
+            "CREATE TABLE " + TABLE_NAME + "("    +
+                    "PLANT_NAME  TEXT, "      +
+                    "PLANT_PIC   TEXT, " +
+                    "WATER_FREQ  INTEGER, "   +
+                    "WATER_AMT   TEXT, "      +
+                    "LIGHT_AMT   TEXT, "      +
+                    "GEN_INFO    TEXT "       + ");";
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     private PlantDatabase( Context context )
@@ -58,9 +56,12 @@ public class PlantDatabase extends SQLiteOpenHelper
         // Not sure if I need to implement this class... ??
     }
 
-    public void insertPlant( String plantName, String plantPic, int waterFreq, Plant.WaterAmt wAmt, Plant.LightAmt lAmt, String genInfo )
+    public void insertPlant( String plantName, int plantPic, int waterFreq, Plant.WaterAmt wAmt, Plant.LightAmt lAmt, String genInfo )
     {
+        Plant p = getPlant( plantName );
+
         ContentValues cv = new ContentValues();
+        Log.i("tag", "insertPlant(): Setting up content values.");
         cv.put( "PLANT_NAME", plantName );
         cv.put( "PLANT_PIC", plantPic );
         cv.put( "WATER_FREQ", waterFreq );
@@ -68,7 +69,39 @@ public class PlantDatabase extends SQLiteOpenHelper
         cv.put( "LIGHT_AMT", lAmt.toString() );
         cv.put( "GEN_INFO", genInfo );
 
-        getWritableDatabase().insert( TABLE_NAME, null, cv );
+        if( p == null )
+        {
+            Log.i("tag", "insertPlant(): Inserting into database: plantName = " + cv.getAsString( "PLANT_NAME" ) );
+            try
+            {
+                if (getWritableDatabase().insert(TABLE_NAME, null, cv) == -1)
+                {
+                    String msg = "insertPlant(): insert() failed.";
+                    Log.i("tag", msg);
+                    throw new SQLiteException(msg);
+                }
+            }
+            catch (SQLiteException se)
+            {
+                Log.i("tag", "insertPlant(): getWritableDatabase() failed - " + se.getMessage());
+                throw se;
+            }
+        }
+        else
+        {
+            Log.i("tag", "insertPlant(): Replacing plant into database: plantName = " + cv.getAsString( "PLANT_NAME" ) );
+            try
+            {
+                getWritableDatabase().update( TABLE_NAME, cv, "PLANT_NAME=\"" + plantName + "\"", null );
+            }
+            catch (SQLiteException se)
+            {
+                Log.i("tag", "insertPlant(): getWritableDatabase() failed 2 - " + se.getMessage());
+                throw se;
+            }
+        }
+
+        Log.i("tag", "insertPlant(): finished.");
     }
 
     /**
@@ -78,21 +111,42 @@ public class PlantDatabase extends SQLiteOpenHelper
     public Plant getPlant( String plantName )
     {
         String[] columns = {"PLANT_PIC", "WATER_FREQ", "WATER_AMT", "LIGHT_AMT", "GEN_INFO"};
-        Cursor c = getReadableDatabase().query( TABLE_NAME, columns, "PLANT_NAME = " + plantName, null, null, null, null );
-        Plant p = new Plant();
-        p.setPlantName( plantName );
+        Plant p          = null;
         try
         {
-            p.setPlantPic( new URL( c.getString(0) ) );
+            SQLiteDatabase db = getReadableDatabase();
+
+            try
+            {
+                Cursor c = db.query(TABLE_NAME, columns, "PLANT_NAME = \"" + plantName + "\"", null, null, null, null);
+
+                if (c.moveToFirst())
+                {
+                    do
+                    {
+                        p = new Plant();
+                        p.setPlantName(plantName);
+                        p.setPlantPic(c.getInt(0));
+                        p.setWaterFreq(c.getInt(1));
+                        p.setWaterAmt(Plant.WaterAmt.valueOf(c.getString(2)));
+                        p.setLightAmt(Plant.LightAmt.valueOf(c.getString(3)));
+                        p.setGenInfo(c.getString(4));
+
+                    } while (c.moveToNext());
+                }
+                c.close();
+            }
+            catch( SQLiteException se )
+            {
+                Log.i( "tag", "PlantDatabase.getPlant(): query() - plant " + plantName + " does not exist: " + se.getMessage() );
+            }
         }
-        catch (MalformedURLException e)
+        catch( SQLiteException se )
         {
-            e.printStackTrace();
+            Log.i( "tag", "PlantDatabase.getPlant(): getReadableDatabase() failed: " + se.getMessage() );
+            throw se;
         }
-        p.setWaterFreq( c.getInt(1) );
-        p.setWaterAmt( Plant.WaterAmt.valueOf( c.getString(2) ) );
-        p.setLightAmt( Plant.LightAmt.valueOf( c.getString(3) ) );
-        p.setGenInfo( c.getString(4) );
+
         return p;
     }
 
@@ -103,15 +157,48 @@ public class PlantDatabase extends SQLiteOpenHelper
     public String[] getAllPlantNames()
     {
         String[] column = {"PLANT_NAME"};
-        Cursor   c      = getReadableDatabase().query( TABLE_NAME, column, null, null, null, null, null );
-        String[] plantNames = new String[c.getCount()];
-        for( int i = 0; i < c.getCount(); i++ )
+        String[] plantNames = null;
+
+        try
         {
-            plantNames[i] = c.getString( i );
+            Log.i( "tag", "PlantDatabase.getAllPlantNames(): Starting query: " );
+            Cursor c = getReadableDatabase().query(TABLE_NAME, column, null, null, null, null, null);
+            Log.i( "tag", "PlantDatabase.getAllPlantNames(): Finished query: " );
+
+            int count = c.getCount();
+
+            Log.i( "tag", "PlantDatabase.getAllPlantNames(): count = " + count + ", column count = " + c.getColumnCount() );
+
+            if( count > 0 )
+            {
+                plantNames = new String[count];
+
+                c.moveToFirst();
+
+                for (int i = 0; i < count; i++)
+                {
+                    plantNames[i] = c.getString(0);
+                    c.moveToNext();
+                    Log.i("tag", "PlantDatabase.getAllPlantNames(): " + i + ") query returned " + plantNames[i]);
+                }
+            }
+            else
+            {
+                Log.i( "tag", "PlantDatabase.getAllPlantNames(): No plants found in the database." );
+            }
+            c.close();
+        }
+        catch( SQLiteException se )
+        {
+            Log.i( "tag", "PlantDatabase.getAllPlantNames(): getReadableDatabase() Failed: " + se.getMessage() );
+            throw se;
+        }
+        catch( Exception e )
+        {
+            Log.i( "tag", "PlantDatabase.getAllPlantNames(): Unhandled Exception: " + e.getMessage() );
+            throw e;
         }
 
         return plantNames;
     }
-
-
 }
